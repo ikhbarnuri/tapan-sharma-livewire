46cd8624d202ebc6112e64cb8e080ab442bd9f69
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-06-20 09:27:49 +0700

    feat: menambahkan package laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::get('/counter', \App\Livewire\Counter::class);
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
